09ff580771b0c01b4edd33f80bf27dd22fb25e0a
stevensharman1 <1stevensharman@gmail.com>
2025-09-29 12:26:52 -0400

    Update fizzbuzz0.js

--- fizzbuzz0.js
+++ fizzbuzz0.js
@@ -1,1 +1,25 @@
+document.getElementById("nameForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  // Get user input
+  const firstName = document.getElementById("first_name").value;
+  const middleInitial = document.getElementById("middle_initial").value;
+  const lastName = document.getElementById("last_name").value;
+
+  // Create greeting
+  const greeting = `Hello, ${firstName} ${middleInitial ? middleInitial + '.' : ''} ${lastName}! Here's your FizzBuzz:`;
+
+  // Generate FizzBuzz
+  let outputHTML = `<h4>${greeting}</h4><ul>`;
+  for (let i = 1; i <= 100; i++) {
+    let result = '';
+    if (i % 3 === 0) result += 'Fizz';
+    if (i % 5 === 0) result += 'Buzz';
+    outputHTML += `<li>${result || i}</li>`;
+  }
+  outputHTML += '</ul>';
+
+  // Display result
+  document.getElementById("output").innerHTML = outputHTML;
+});
 
